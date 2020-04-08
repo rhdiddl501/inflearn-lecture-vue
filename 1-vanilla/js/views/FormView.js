5dd3423299ef2cfba1e6952e6f1958854aa0183f
hjh <rhdiddl501@naver.com>
2020-04-08 13:11:59 +0900

    검색폼 구현-4

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -20,12 +20,19 @@ FormView.showResetBtn = function (show = true) {
 FormView.bindEvents = function () {
     this.on('submit', e => e.preventDefault())
     this.inputEl.addEventListener('keyup', e => this.onKeyUp(e))
+    this.resetEl.addEventListener('click', e => this.onClickReset())
 }
 
 FormView.onKeyUp = function (e) {
     const enter = 13
     this.showResetBtn(this.inputEl.value.length)
     if(e.keyCode === enter) this.emit('@submit', {input: this.inputEl.value})
+    if(!this.inputEl.value.length) this.emit('@reset')
+}
+
+FormView.onClickReset = function() {
+    this.emit('@reset')
+    this.showResetBtn(false)
 }
 
 export default FormView
