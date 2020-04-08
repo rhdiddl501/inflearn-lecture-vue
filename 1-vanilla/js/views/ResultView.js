324a4a8b7f648f62dcbffb378a24a37ebda377c3
hjh <rhdiddl501@naver.com>
2020-04-08 16:27:36 +0900

    검색결과 구현-2

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -2,17 +2,30 @@ import View from './View.js'
 
 const ResultView = Object.create(View)
 
+ResultView.messages = {
+    NO_RESULT: 'No Data'
+}
+
 ResultView.setup = function(el) {
     this.init(el)
-
 }
 
 ResultView.render = function (data = []) {
-    this.el.innerHTML = data.length? this.getSearchResult(data) : 'No Data'
+    this.el.innerHTML = data.length? this.getSearchResultHtml(data) : this.messages.NO_RESULT
 }
 
-ResultView.getSearchResult = function (data) {
-    debugger
+ResultView.getSearchResultHtml = function (data) {
+    return data.reduce((html,item) => {
+       html += this.getSearchItemHtml(item)
+       return html
+    }, '<ul>') + '</ul>' 
 }
+
+ResultView.getSearchItemHtml = function (item) {
+    return `<li>
+      <img src="${item.image}" />
+      <p>${item.name}</p>
+    </li>`
+  }
 
 export default ResultView
