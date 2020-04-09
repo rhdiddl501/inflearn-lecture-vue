d17f094378a96f5ea777e35ba33243be71bb53e6
hjh <rhdiddl501@naver.com>
2020-04-09 13:29:18 +0900

    최근검색어구현-3

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -2,5 +2,15 @@ import KeywordView from './KeywordView.js'
 
 const HistoryView = Object.create(KeywordView)
 
+HistoryView.getKeywordHtml = function (data) {
+    return data.reduce((html,item) => {
+        html += `<li data-keyword="${item.keyword}">
+        ${item.keyword}
+        <span class="date">${item.date}</span>
+        <button class="btn-remove"></button>
+        </li>`
+        return html
+    },'<ul class="list">') + '</ul>'
+}
 
 export default HistoryView
