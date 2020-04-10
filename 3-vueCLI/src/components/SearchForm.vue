15b5ba290bfc8279893bf4e68fe3bcacb6c68c6c
hjh <rhdiddl501@naver.com>
2020-04-10 11:48:30 +0900

    검색폼구현-1

---
+++ 3-vueCLI/src/components/SearchForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <input v-model="query" type="text" placeholder="검색어를 입력해 주세요">
+        <button v-show="isShowBtn" type="reset" class="btn-reset" v-on:click="reset()"/>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: '',
+            isShowBtn: false
+        }
+    },
+    watch: {
+        query() {
+            if(this.query.length === 0) {
+                this.isShowBtn = false
+            }
+        }
+    },
+    method: {
+        reset() {
+            this.query = ''
+        }
+    }
+}
+</script>
