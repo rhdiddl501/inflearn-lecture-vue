060f68ad6533d8839b20213b83adcfc440b9476e
hjh <rhdiddl501@naver.com>
2020-04-10 14:37:07 +0900

    tab -1

--- 3-vueCLI/src/App.vue
+++ 3-vueCLI/src/App.vue
@@ -1,45 +1,54 @@
 <template>
-<div>
-  <header>
-    <h2 class="container">검색</h2>
-  </header>
+  <div>
+    <header>
+      <h2 class="container">검색</h2>
+    </header>
 
-  <SearchComponent :searchVal="searchValue" v-on:@search="search"
-    v-on:@reset="reset"></SearchComponent>
-  <div v-if="submitted">
-  <SearchResultComponent :datas="searchResultData"></SearchResultComponent>
-  </div> 
-</div>
+    <div class="container">
+      <SearchComponent :searchVal="searchValue" v-on:@search="search" v-on:@reset="reset"></SearchComponent>
+
+      <div class="content">
+        <div v-if="submitted">
+          <SearchResultComponent :datas="searchResultData"></SearchResultComponent>
+        </div>
+
+        <TabComponent />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import SearchComponent from './components/SearchComponent.vue'
-import SearchResultComponent from './components/ResultComponent.vue'
+import SearchComponent from "./components/SearchComponent.vue";
+import SearchResultComponent from "./components/ResultComponent.vue";
+import TabComponent from "./components/TabComponent.vue";
 
-import SearchModel from './models/SearchModel.js'
+import SearchModel from "./models/SearchModel.js";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      searchValue: '',
+      searchValue: "",
       submitted: false,
       searchResultData: []
-    }
+    };
   },
   components: {
-    SearchComponent, SearchResultComponent
+    SearchComponent,
+    SearchResultComponent,
+    TabComponent
   },
   methods: {
     search(searchVal) {
-      this.searchValue = searchVal
+      this.searchValue = searchVal;
       SearchModel.list().then(data => {
-        this.searchResultData = data
-      })
-      this.submitted = true
+        this.searchResultData = data;
+      });
+      this.submitted = true;
     },
     reset() {
-      this.submitted = false
+      this.submitted = false;
     }
   }
-}
+};
 </script>
---
+++ 3-vueCLI/src/components/TabComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="tabs">
+        <li v-for="tab in tabs" :key="tab">
+            {{tab}}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            tabs: ['추천 검색어','최근검색어']
+        }
+    }
+}
+</script>
